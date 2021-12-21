389194d42a26bffb7d9f77daa296b9613c50b6f7
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2021-12-22 00:04:04 +0100

    Add ZHAOpenClose device type

--- deconz.py
+++ deconz.py
@@ -2,57 +2,76 @@ from prometheus_client import Gauge
 import json
 
 _gauges = {
-  "battery": Gauge("deconz_sensor_battery", "Battery level of sensor in percent", ["manufacturer", "model", "name"]),
-  "humidity": Gauge("deconz_sensor_humidity", "Humidity of sensor in percent", ["manufacturer", "model", "name", "type", "uid"]),
-  "pressure": Gauge("deconz_sensor_pressure", "Air pressure in hectopascal (hPa)", ["manufacturer", "model", "name", "type", "uid"]),
-  "temperature": Gauge("deconz_sensor_temperature", "Temperature of sensor in Celsius", ["manufacturer", "model", "name", "type", "uid"]),
-};
+    "battery": Gauge("deconz_sensor_battery", "Battery level of sensor in percent", ["manufacturer", "model", "name"]),
+    "humidity": Gauge("deconz_sensor_humidity", "Humidity of sensor in percent", ["manufacturer", "model", "name", "type", "uid"]),
+    "pressure": Gauge("deconz_sensor_pressure", "Air pressure in hectopascal (hPa)", ["manufacturer", "model", "name", "type", "uid"]),
+    "temperature": Gauge("deconz_sensor_temperature", "Temperature of sensor in Celsius", ["manufacturer", "model", "name", "type", "uid"]),
+    "open": Gauge("deconz_sensor_open", "Door status", ["manufacturer", "model", "name", "type", "uid"]),
+}
 
 _functionMap = {
-  'ZHAHumidity': lambda x: _extract_basic_metric(x, 'humidity', 100),
-  'ZHATemperature': lambda x: _extract_basic_metric(x, 'temperature', 100),
-  'ZHAPressure': lambda x: _extract_basic_metric(x, 'pressure', 1),
-};
+    'ZHAHumidity': lambda x: _extract_basic_metric(x, 'humidity', 100),
+    'ZHATemperature': lambda x: _extract_basic_metric(x, 'temperature', 100),
+    'ZHAPressure': lambda x: _extract_basic_metric(x, 'pressure', 1),
+    'ZHAOpenClose': lambda x: _extract_basic_metric_OpenClose(x, 'open'),
+}
 
 
 def _extract_basic_metric(metric, metricName, divider):
-    value = float(metric['state'][metricName])/divider;
+    value = float(metric['state'][metricName])/divider
 
     _gauges[metricName].labels(
-      manufacturer = metric['manufacturername'], 
-      model=metric['modelid'], 
-      name=metric['name'], 
-      type=metric['type'],
-      uid=metric['uniqueid'],
-    ).set(value);
-    
-def extract_metrics(logger, request_content):
-  data = json.loads(request_content.decode('utf-8'));
-  _extract_battery(data);
+        manufacturer=metric['manufacturername'],
+        model=metric['modelid'],
+        name=metric['name'],
+        type=metric['type'],
+        uid=metric['uniqueid'],
+    ).set(value)
 
-  for key in data:
-    metric = data[key];
-    
-    if metric['type'] in _functionMap:
-      _functionMap[metric['type']](metric);
+
+def _extract_basic_metric_OpenClose(metric, metricName):
+    if metric['state'][metricName] == 'true':
+        value = 1
     else:
-      logger.info(f"Unknow metric type \"{metric['type']}\".");
+        value = 0
+
+    _gauges[metricName].labels(
+        manufacturer=metric['manufacturername'],
+        model=metric['modelid'],
+        name=metric['name'],
+        type=metric['type'],
+        uid=metric['uniqueid'],
+    ).set(value)
+
+
+def extract_metrics(logger, request_content):
+    data = json.loads(request_content.decode('utf-8'))
+    _extract_battery(data)
+
+    for key in data:
+        metric = data[key]
+
+        if metric['type'] in _functionMap:
+            _functionMap[metric['type']](metric)
+        else:
+            logger.info(f"Unknow metric type \"{metric['type']}\".")
+
 
 def _extract_battery(data):
-  processed = set();
+    processed = set()
 
-  for key in data:
-    value = 0;
-    metric = data[key];
+    for key in data:
+        value = 0
+        metric = data[key]
 
-    if metric['name'] in processed:
-      continue;
-    
-    processed.add(metric['name']);
-    config = metric['config'];
+        if metric['name'] in processed:
+            continue
 
-    if 'battery' in config:
-      value = int(config['battery']);
-    
-    _gauges['battery'].labels(manufacturer = metric['manufacturername'], model=metric['modelid'], name=metric['name']).set(value);
+        processed.add(metric['name'])
+        config = metric['config']
 
+        if 'battery' in config:
+            value = int(config['battery'])
+
+        _gauges['battery'].labels(manufacturer=metric['manufacturername'],
+                                  model=metric['modelid'], name=metric['name']).set(value)
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import logging
 import time
 import sys
 import os
-import deconz;
-import signal;
+import deconz
+import signal
 from threading import Event
 
 """
@@ -16,78 +16,84 @@ DECONZ_URL      Sets deconz url, default is 'localhost'
 DECONZ_TOKEN    Sets deconz token, default is ''
 UPDATE_INTERVAL Sets interval between updates in seconds, default is 10.0 seconds
 """
-POST_LABLE = 'HOST_PORT';
-DECONZ_PORT_LABLE = 'DECONZ_PORT';
-URL_LABLE = 'DECONZ_URL';
-TOKEN_LABLE = 'DECONZ_TOKEN';
-TIMEOUT_LABLE = 'UPDATE_INTERVAL';
+POST_LABLE = 'HOST_PORT'
+DECONZ_PORT_LABLE = 'DECONZ_PORT'
+URL_LABLE = 'DECONZ_URL'
+TOKEN_LABLE = 'DECONZ_TOKEN'
+TIMEOUT_LABLE = 'UPDATE_INTERVAL'
 
-exit = Event();
+exit = Event()
+
 
 def signalShuttdown(self, *args):
-  exit.set();
+    exit.set()
+
 
 config = {
-  'target_port': 80,
-  'host_port': 80,
-  'url': 'localhost',
-  'token': '',
-  'timeout': 10.0
-};
+    'target_port': 80,
+    'host_port': 80,
+    'url': 'localhost',
+    'token': '',
+    'timeout': 10.0
+}
 
 if POST_LABLE in os.environ:
-  config['host_port'] = int(os.environ[POST_LABLE]);
+    config['host_port'] = int(os.environ[POST_LABLE])
 
 if DECONZ_PORT_LABLE in os.environ:
-  config['target_port'] = int(os.environ[DECONZ_PORT_LABLE]);
+    config['target_port'] = int(os.environ[DECONZ_PORT_LABLE])
 
 if URL_LABLE in os.environ:
-  config['url'] = os.environ[URL_LABLE];
+    config['url'] = os.environ[URL_LABLE]
 
 if TOKEN_LABLE in os.environ:
-  config['token'] = os.environ[TOKEN_LABLE].strip();
+    config['token'] = os.environ[TOKEN_LABLE].strip()
 
 if TIMEOUT_LABLE in os.environ:
-  config['timeout'] = float(os.environ[TIMEOUT_LABLE]);
+    config['timeout'] = float(os.environ[TIMEOUT_LABLE])
+
 
 def create_logger(scope):
-  logger = logging.getLogger(scope)
-  handler = logging.StreamHandler(sys.stdout)
-  handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt="%Y-%m-%dT%H:%M:%S"))
-  logger.addHandler(handler)
-  logger.setLevel(logging.INFO)
-  return logger
+    logger = logging.getLogger(scope)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt="%Y-%m-%dT%H:%M:%S"))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
 
 if __name__ == '__main__':
-  logger = create_logger('deconz-exporter');
+    logger = create_logger('deconz-exporter')
 
-  if not config['token']:
-    logger.error(f"No deconz token provided.");
-    exit(1);
+    if not config['token']:
+        logger.error(f"No deconz token provided.")
+        exit(1)
 
-  start_http_server(config['host_port']);
-  connection = client.HTTPConnection(config['url'], config['target_port']);
-  path = "/api/{}/sensors".format(config['token']);
+    start_http_server(config['host_port'])
+    connection = client.HTTPConnection(config['url'], config['target_port'])
+    path = "/api/{}/sensors".format(config['token'])
 
-  signal.signal(signal.SIGTERM, signalShuttdown);
-  signal.signal(signal.SIGHUP, signalShuttdown);
-  signal.signal(signal.SIGINT, signalShuttdown);
-  signal.signal(signal.SIGABRT, signalShuttdown);
+    signal.signal(signal.SIGTERM, signalShuttdown)
+    signal.signal(signal.SIGHUP, signalShuttdown)
+    signal.signal(signal.SIGINT, signalShuttdown)
+    signal.signal(signal.SIGABRT, signalShuttdown)
 
-  while not exit.is_set():
-    connection.request("GET", path);
-    response = connection.getresponse();
+    while not exit.is_set():
+        connection.request("GET", path)
+        response = connection.getresponse()
 
-    if response.status == 200:
-      deconz.extract_metrics(logger, response.read());
-      logger.info(f"Request succeeded");
-    else:
-      logger.error(f"Request did not result in a successful status, {response.status} {response.reason}.");
+        if response.status == 200:
+            deconz.extract_metrics(logger, response.read())
+            logger.info(f"Request succeeded")
+        else:
+            logger.error(
+                f"Request did not result in a successful status, {response.status} {response.reason}.")
 
-    sleepTime = 0.0;
+        sleepTime = 0.0
 
-    while (config['timeout'] > sleepTime) and not exit.is_set():
-      time.sleep(0.1);
-      sleepTime += 0.1;
+        while (config['timeout'] > sleepTime) and not exit.is_set():
+            time.sleep(0.1)
+            sleepTime += 0.1
 
-  logger.info("shutting down");
+    logger.info("shutting down")
